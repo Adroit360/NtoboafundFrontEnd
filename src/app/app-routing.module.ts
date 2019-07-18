45cc7aebd88e699ec214e78227ac29963194254f
Justice <justice.addico.ja@outlook.com>
2019-07-18 12:38:27 +0000

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"login",component:LoginComponent},
   {path:"register",component:RegisterComponent},
-  {path:"luckyme/:checkoutId?",component:LuckymeComponent},
+  {path:"luckyme",component:LuckymeComponent},
   {path:"tnxluckyme",component:TnxluckymeComponent}
 
 ];
