5ab571853edbbbf6f86f561f9f1c37825a6483dd
Justice <justice.addico.ja@outlook.com>
2019-08-15 12:33:28 +0000

    pointed to azure

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,7 @@
 var apiUrl:"https://ntoboafund.gear.host",
  LocalApiUrl:"https://localhost:44311",
- localKestrelUrl:"https://localhost:5001";
+ localKestrelUrl:"https://localhost:5001",
+ azure:"http://ntoboafundwebapi.azurewebsites.net";
 export const settings = {
-    currentApiUrl:"https://ntoboafund.gear.host"
+    currentApiUrl:"http://ntoboafundwebapi.azurewebsites.net"
 } 
